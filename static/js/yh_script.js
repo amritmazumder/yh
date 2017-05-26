9ad26b7fa80c0c58596412403b6bd970ac63f0cc
Amrit Mazumder <amrit.mazumder@gmail.com>
2017-05-25 22:31:20 -0700

    First Commit
    — with love, efco

---
+++ static/js/yh_script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// $('.nav-header').css({
+	// 	'position':'fixed',
+	// 	'z-index':'9999',
+	// 	'top':'0'
+	// });
+
+	$(window).on('scroll', function(){	
+		var scrollTop = $('body').scrollTop();
+		//console.log(scrollTop);
+		if (scrollTop > 40) {		
+			
+		} else {
+			
+			
+		}
+	});
+});
